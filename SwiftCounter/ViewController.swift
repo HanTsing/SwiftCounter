4708aa3a0b12543f3e907b2d3752a92d090cc709
hanqing <tsinghan07@gmail.com>
2014-11-20 09:39:47 +0800

    Initial Commit

---
+++ SwiftCounter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftCounter
+//
+//  Created by hanqing on 11/20/14.
+//  Copyright (c) 2014 hanqing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
